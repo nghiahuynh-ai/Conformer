1174f2ebe74fb3674be34aa6a3b96b01673a3665
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-03 17:57:16 +0700

    fix encoder

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -90,19 +90,19 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
             num_classes=self.joint.num_classes_with_blank - 1, loss_name=loss_name, loss_kwargs=loss_kwargs
         )
         
-        subsampling = self._cfg.encoder.subsampling
-        subsampling_factor = self._cfg.encoder.subsampling_factor
-        feat_in = self._cfg.encoder.feat_in
-        d_model = self._cfg.encoder.d_model
-        subsampling_conv_channels = d_model
-        self.subsampling = ConvSubsampling(
-            subsampling=subsampling,
-            subsampling_factor=subsampling_factor,
-            feat_in=feat_in,
-            feat_out=d_model,
-            conv_channels=subsampling_conv_channels,
-            activation=nn.ReLU(),
-        )
+        # subsampling = self._cfg.encoder.subsampling
+        # subsampling_factor = self._cfg.encoder.subsampling_factor
+        # feat_in = self._cfg.encoder.feat_in
+        self.d_model = self._cfg.encoder.d_model
+        # subsampling_conv_channels = d_model
+        # self.subsampling = ConvSubsampling(
+        #     subsampling=subsampling,
+        #     subsampling_factor=subsampling_factor,
+        #     feat_in=feat_in,
+        #     feat_out=d_model,
+        #     conv_channels=subsampling_conv_channels,
+        #     activation=nn.ReLU(),
+        # )
 
         if hasattr(self.cfg, 'spec_augment') and self._cfg.spec_augment is not None:
             self.spec_augmentation = EncDecRNNTModel.from_config_dict(self.cfg.spec_augment)
@@ -687,10 +687,12 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
             processed_signal, processed_signal_length = self.preprocessor(
                 input_signal=input_signal, length=input_signal_length,
             )
-        print("===================", processed_signal.shape, "=========================")
-        raise
-        processed_signal, processed_signal_length = self.subsampling(processed_signal, processed_signal_length)
-            
+
+        # processed_signal, processed_signal_length = self.subsampling(processed_signal, processed_signal_length)
+        _, _, in_feature = processed_signal.shape
+        in_proj = nn.Linear(in_feature, self.d_model)
+        processed_signal = in_proj(processed_signal)
+        
         self.origin_input = processed_signal
         
         # Spec augment is not applied during evaluation/testing
