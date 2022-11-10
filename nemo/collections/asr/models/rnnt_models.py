639c329c419265ecfae03ae351eefdfb77e13df2
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-10 08:23:20 +0700

    add alignment masking

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -667,7 +667,7 @@ class EncDecRNNTModel(ASRModel, ASRModuleMixin, Exportable):
             processed_signal = self.spec_augmentation(input_spec=processed_signal, length=processed_signal_length)
         
         encoded, encoded_len = self.encoder(audio_signal=processed_signal, length=processed_signal_length)
-        print(signal)
+        print(encoded)
         raise 
         return encoded, encoded_len
 
