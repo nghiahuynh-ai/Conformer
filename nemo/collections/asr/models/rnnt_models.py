73d93d5533a0fbc9edaf2113f0148f359318e231
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-17 22:26:17 +0700

    modify aligment-mask

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -1002,6 +1002,8 @@ class AlignmentMask(nn.Module):
             transcript[idx] = new_text
                 
             for i in mask:
+                print(i)
+                raise
                 if i < start_idx.shape[0] - 1:
                     signal[idx][start_idx[i]: start_idx[i+1]] = 0.0
                 else:
