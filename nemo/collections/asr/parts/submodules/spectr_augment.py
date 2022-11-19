f1237160ccf8a6ccccbb32f4df57dab5a71fbd42
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-19 18:39:17 +0700

    modify aligment-mask

--- nemo/collections/asr/parts/submodules/spectr_augment.py
+++ nemo/collections/asr/parts/submodules/spectr_augment.py
@@ -182,3 +182,33 @@ class SpecShot(nn.Module, Typing):
         mask = prob > self.mask_ratio
         input_spec = input_spec * mask.to(input_spec.device)
         return input_spec
+    
+    
+class FreqNoise(nn.Module, Typing):
+    
+    @property
+    def input_types(self):
+        """Returns definitions of module input types
+        """
+        return {"input_spec": NeuralType(('B', 'D', 'T'), SpectrogramType())}
+
+    @property
+    def output_types(self):
+        """Returns definitions of module output types
+        """
+        return {"augmented_spec": NeuralType(('B', 'D', 'T'), SpectrogramType())}
+    
+    def __init__(self, mask_ratio=0.3, mask_value=0.0):
+        super().__init__()
+
+        self.mask_ratio = mask_ratio
+        self.mask_value = mask_value
+
+    @typecheck()
+    @torch.no_grad()
+    def forward(self, input_spec):
+        batch_size, freq_size, time_size = input_spec.shape
+        prob = -1.0 * torch.rand(batch_size, freq_size, time_size) + 1.0
+        mask = prob > self.mask_ratio
+        input_spec = input_spec * mask.to(input_spec.device)
+        return input_spec
