24999a90ed493bda8eeb87ec71ac507f843407c9
nghiahuynh <huynhnguyenhieunghia1999@gmail.com>
2022-11-24 12:51:32 +0700

    update

--- nemo/collections/asr/models/rnnt_models.py
+++ nemo/collections/asr/models/rnnt_models.py
@@ -1008,6 +1008,7 @@ class AlignmentMask(nn.Module):
             num_words = len(start)
             num_masks = int(ratio * num_words)
             mask_prob = -1.0 * score.cpu().detach().numpy() + 1.0
+            mask_prob = mask_prob / sum(mask_prob)
             mask = np.random.choice(range(num_words), size=num_masks, replace=False, p=mask_prob)
 
             # t = transcript[b]
